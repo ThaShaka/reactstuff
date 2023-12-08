cacfaa09d3da69cf0af86e2d52c8f62ed17b890a
Karlo Pervan <karlopervan4@gmail.com>
2023-12-08 17:49:57 +0000

    asdasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 function App() {
-  const test = (
-    <div>
-      <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore, accusamus?</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia magni placeat repudiandae. Rem mollitia dolorem commodi quis, neque sequi natus ipsa architecto blanditiis beatae necessitatibus dicta enim! Rerum et consequuntur fuga placeat, explicabo nisi maxime illum minima repellendus voluptatum ullam!</p>
-      <p>Impedit aut tenetur accusantium placeat pariatur assumenda reprehenderit est harum omnis rem nobis maxime incidunt excepturi illum animi cumque corrupti, dolorum recusandae eveniet quia accusamus atque. Voluptas similique ipsum totam veniam facere vitae eligendi, ea pariatur impedit. Nihil, nulla mollitia.</p>
-    </div>
-  )
   
-
   return (
-      test
+    <div>
+      <img src="../public/react-logo.png" alt="" width={"40px"} />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, inlucding mobile apps</li>
+      </ul>
+    </div>
   )
 }
 
