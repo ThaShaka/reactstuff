8dd58e29c5a447d2aee12c97c3297133da32b1e2
Karlo Pervan <karlopervan4@gmail.com>
2023-12-28 12:49:44 +0100

    FALAKURUC

--- 50projects/ProgressSteps/src/App.jsx
+++ 50projects/ProgressSteps/src/App.jsx
@@ -2,42 +2,29 @@ import {useState} from "react";
 import Step from "./Step.jsx";
 
 function App() {
-    const [step, setStep] = useState([{
-        step:1,
-        isActive: true
-    },
-        {
-            step:2,
-            isActive: true,
-        },
-        {
-            step: 3,
-            isActive: false
-        },
-        {
-            step: 4,
-            isActive: false
-        }
-    ])
-    function increaseStep() {
+    const [steps, setStep] = useState(0)
+    const [state, setState] = useState([true])
+    let styles = {
+        width:  `${steps*33}%`,
+    }
+    function increaseStep(){
         setStep(prevState => prevState + 1)
-        console.log(step)
+        setState(prevState => [...prevState, true])
     }
     function decreaseStep() {
-        setStep(prevState =>  prevState - 1)
-        console.log(step)
+        setStep(prevState => prevState - 1)
+        setState(prevState => prevState.slice(0, -1))
     }
-    return <main>
+        return <main>
         <div className="progress-container">
-            <div className="progress"></div>
-            {step.map((step,index)=> {return <Step key={index} value={step.step} isActive={step.isActive}/>})}
+            <div className="progress" style={styles} ></div>
+            {[1,2,3,4].map((step,index)=> {return <Step key={index} value={step} isActive={state[index]}/>})}
         </div>
         <section className="button">
-            <button className={"btn"} disabled={step<=1} onClick={decreaseStep}>Prev</button>
-            <button className="btn next" onClick={increaseStep} disabled={step>4}>Next</button>
+            <button className={"btn"} disabled={steps<1} onClick={decreaseStep}>Prev</button>
+            <button className="btn next" onClick={increaseStep} disabled={steps>2}>Next</button>
         </section>
     </main>
-
 
 }
 
